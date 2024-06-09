d8a1bc455551390970e6f4ace0c0380324ef5c86
Fachrifat <fachrifat05@gmail.com>
2024-06-09 15:09:33 +0700

    coba ini apakah 100%

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -15,30 +15,20 @@ const Footer = () => {
                     <h1 className="lg:text-2xl">Jl. Balai Pustaka Baru I No.2, RT.2/RW.7, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</h1>
                 </div>
                 <div className="flex px-6 gap-2.5 mb-7 lg:justify-between lg:mb-20">
-                    <IsiFooter 
-                    judul_isi="LAMAN SITUS" 
-                    isi1="Beranda" 
-                    link1="/#home"
-                    isi2="Berita" 
-                    link2="/#berita"
-                    isi3="Data"
-                    link3="/#data"
-                    isi4="Edukasi"
-                    link4="/#edukasi"
-                    isi5="Kalkulator"
-                    link5="/kalkulator" />
+                    <IsiFooter judul_isi="LAMAN SITUS" isi1="Beranda" link1="/#home" isi2="Berita" link2="/#berita" isi3="Data" link3="/#data" isi4="Edukasi" link4="/#edukasi" isi5="Kalkulator" link5="/kalkulator" />
                     <span className="bg-white w-[0.8px]"></span>
-                    <IsiFooter judul_isi="SUMBER DATA"
-                    isi1="Sektor Energi"
-                    link1="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_energy_p"
-                    isi2="Sektor IPPU"
-                    link2="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_ippu_p"
-                    isi3="Sektor Pertanian"
-                    link3="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_agriculture_p"
-                    isi4="Sektor Kehutanan"
-                    link4="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_forestry_p"
-                    isi5="Sektor Limbah"
-                    link5="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_waste_p"
+                    <IsiFooter
+                        judul_isi="SUMBER DATA"
+                        isi1="Sektor Energi"
+                        link1="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_energy_p"
+                        isi2="Sektor IPPU"
+                        link2="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_ippu_p"
+                        isi3="Sektor Pertanian"
+                        link3="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_agriculture_p"
+                        isi4="Sektor Kehutanan"
+                        link4="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_forestry_p"
+                        isi5="Sektor Limbah"
+                        link5="https://signsmart.menlhk.go.id/v2.1/app/chart/emisi_waste_p"
                     />
                     <span className="bg-white w-[0.8px]"></span>
                     <IsiFooter judul_isi="CREDIT" isi1="Freepik" />
@@ -53,8 +43,12 @@ const Footer = () => {
                     <button className="absolute font-semibold right-10 bottom-4 bg-hijau px-6 py-2 rounded-lg">KIRIM</button>
                 </div>
                 <div className="flex justify-center items-center gap-4 pb-10">
-                    <img src={Instagram} alt="" />
-                    <img src={Email} alt="" />
+                    <a href="https://www.instagram.com/zerocardn.id/" target="_blank">
+                        <img src={Instagram} alt="" />
+                    </a>
+                    <a href="mailto:zerocardn.id@gmail.com?subject=Subjek Email&body=Isi Pesan" target="_blank">
+                        <img src={Email} alt="" />
+                    </a>
                 </div>
             </div>
             <div className="bg-[#57AE09] h-20 flex justify-center items-center">
--- src/Path/PathKalkulator.jsx
+++ src/Path/PathKalkulator.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../Components/Fragments/Navbar"
 import Kalkulator from '../Pages/Kalkulator'
 import Footer from "../Pages/Footer"
+import Gordeng from "../Pages/Gordeng"
 
 
 const PathKalkulator = () => {
   return (
     <div>
+      <Gordeng />
       <Navbar/>
       <Kalkulator/>
       <Footer/>
